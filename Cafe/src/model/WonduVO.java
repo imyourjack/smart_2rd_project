5d9271e64645c4a4bf0fb43ecdc8351007cd027a
DESKTOP-A2NVQUE\21SMT13 <asd@asd.com>
2021-06-26 14:28:20 +0900

    근아2

---
+++ Cafe/src/model/WonduVO.java
@@ -0,0 +1,31 @@
+package model;
+
+public class WonduVO {
+     
+   private int reco_index;
+   private String wondu_n;
+   private String user_idx;
+   
+   public int getReco_index() {
+      return reco_index;
+   }
+   public void setReco_index(int reco_index) {
+      this.reco_index = reco_index;
+   }
+   public String getWondu_n() {
+      return wondu_n;
+   }
+   public void setWondu_n(String wondu_n) {
+      this.wondu_n = wondu_n;
+   }
+   public String getUser_idx() {
+      return user_idx;
+   }
+   public void setUser_idx(String user_idx) {
+      this.user_idx = user_idx;
+   }
+   
+   
+   
+   
+}
